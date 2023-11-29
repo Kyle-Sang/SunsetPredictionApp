e4ceacc838cd9019d50f113cf06c46c57cad745e
zsmith1301 <93056327+zsmith1301@users.noreply.github.com>
2023-11-28 19:41:39 -0500

    Added empty files for activities and layouts

---
+++ SunsetPrediction/app/src/main/java/com/example/sunsetprediction/LocalInfoActivity.kt
@@ -0,0 +1,4 @@
+package com.example.sunsetprediction
+
+class LocalInfoActivity {
+}
--- SunsetPrediction/app/src/main/java/com/example/sunsetprediction/MapActivity.kt
+++ SunsetPrediction/app/src/main/java/com/example/sunsetprediction/MapActivity.kt
@@ -3,7 +3,7 @@ package com.example.sunsetprediction
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
