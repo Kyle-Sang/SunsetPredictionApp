5ed1f4bd3f05fe9e410695766b1a3e5019b4973b
unknown <rmondal@terpmail.umd.edu>
2023-12-05 17:54:33 -0500

    added activity switching

--- SunsetPrediction/app/src/main/java/com/example/sunsetprediction/LocalInfoActivity.kt
+++ SunsetPrediction/app/src/main/java/com/example/sunsetprediction/LocalInfoActivity.kt
@@ -1,4 +1,20 @@
 package com.example.sunsetprediction
 
-class LocalInfoActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class LocalInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_local_info)
+    }
+
+    fun nextScreen(v : View) {
+        var intent: Intent = Intent(this, RatingActivity::class.java)
+        startActivity(intent) // go to Travel Activity
+        Log.w("MapActivity", "to rating")
+    }
 }
--- SunsetPrediction/app/src/main/java/com/example/sunsetprediction/RatingActivity.kt
+++ SunsetPrediction/app/src/main/java/com/example/sunsetprediction/RatingActivity.kt
@@ -1,4 +1,21 @@
 package com.example.sunsetprediction
 
-class RatingActivity {
+import android.os.Bundle
+import android.util.Log
+import android.widget.RatingBar
+import androidx.appcompat.app.AppCompatActivity
+
+class RatingActivity : AppCompatActivity() {
+    private lateinit var rt : RatingBar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rating)
+
+        rt = findViewById(R.id.ratingBar);
+        var rating = rt.numStars
+        Log.w("RatingActivity", rating.toString())
+
+        // store via persistent data
+        // send data to server (S3 bucket) once page is closed
+    }
 }
