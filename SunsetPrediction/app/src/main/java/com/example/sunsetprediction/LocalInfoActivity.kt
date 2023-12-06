86894f846496858da0c93ed8c126398f1ad0c1d7
unknown <rmondal@terpmail.umd.edu>
2023-12-05 22:43:20 -0500

    shared preferences and routing

--- SunsetPrediction/app/src/main/java/com/example/sunsetprediction/LocalInfoActivity.kt
+++ SunsetPrediction/app/src/main/java/com/example/sunsetprediction/LocalInfoActivity.kt
@@ -1,26 +1,36 @@
 package com.example.sunsetprediction
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AnalogClock
+import android.widget.RatingBar
 import androidx.appcompat.app.AppCompatActivity
 
 class LocalInfoActivity : AppCompatActivity() {
     private lateinit var clock : AnalogClock
+    private lateinit var pred_rating : RatingBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_local_info)
 
         clock = findViewById(R.id.clock)
-
+        pred_rating = findViewById(R.id.rating)
+        pred_rating.rating = getRating()
     }
 
     fun nextScreen(v : View) {
         var intent: Intent = Intent(this, RatingActivity::class.java)
         startActivity(intent) // go to Travel Activity
         Log.w("MapActivity", "to rating")
+    }
+
+    fun returnToMap(v : View) {
+        finish()
+    }
+
+    private fun getRating(): Float {
+        return (0..5).random().toFloat()
     }
 }
--- SunsetPrediction/app/src/main/java/com/example/sunsetprediction/RatingActivity.kt
+++ SunsetPrediction/app/src/main/java/com/example/sunsetprediction/RatingActivity.kt
@@ -1,21 +1,57 @@
 package com.example.sunsetprediction
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.RatingBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class RatingActivity : AppCompatActivity() {
     private lateinit var rt : RatingBar
+    private lateinit var ratings_count : TextView
+    private var num_user_ratings : Int = 0
+    private var rating : Float = 0f
+    private lateinit var editor : SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rating)
 
-        rt = findViewById(R.id.ratingBar);
-        var rating = rt.numStars
-        Log.w("RatingActivity", rating.toString())
+        rt = findViewById(R.id.ratingBar)
+        ratings_count = findViewById(R.id.ratings_count)
 
+        var pref : SharedPreferences= this.getSharedPreferences(this.packageName + "_preferences",
+            Context.MODE_PRIVATE)
+        editor = pref.edit()
+
+        num_user_ratings = pref.getInt(PREFERENCE_NUM_RATINGS, 0)
+        ratings_count.text = "Total ratings contributed ${num_user_ratings}"
         // store via persistent data
         // send data to server (S3 bucket) once page is closed
+    }
+
+    fun saveRating(v : View) {
+        Log.w("RatingActivity", "save rating")
+        rating = rt.rating
+        setPreferences()
+        Log.w("RatingActivity", rating.toString())
+        finish()
+    }
+
+    // stores number of ratings
+    private fun setPreferences() {
+        Log.w("RatingActivity", "set preferences")
+        // gets previous number of user ratings
+        // updates value to include new rating
+        num_user_ratings += 1
+        editor.putInt(PREFERENCE_NUM_RATINGS, num_user_ratings)
+        editor.commit()
+    }
+
+    companion object {
+        private const val PREFERENCE_NUM_RATINGS : String = "rating"
     }
 }
