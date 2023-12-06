97d6ae23976e74608581542860378dc9f9c3d1a9
unknown <rmondal@terpmail.umd.edu>
2023-12-05 19:05:17 -0500

    location info components added

--- SunsetPrediction/app/src/main/java/com/example/sunsetprediction/LocalInfoActivity.kt
+++ SunsetPrediction/app/src/main/java/com/example/sunsetprediction/LocalInfoActivity.kt
@@ -1,15 +1,21 @@
 package com.example.sunsetprediction
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AnalogClock
 import androidx.appcompat.app.AppCompatActivity
 
 class LocalInfoActivity : AppCompatActivity() {
+    private lateinit var clock : AnalogClock
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_local_info)
+
+        clock = findViewById(R.id.clock)
+
     }
 
     fun nextScreen(v : View) {
